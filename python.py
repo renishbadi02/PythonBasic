b135934b3062d0c3989dec0ff8af751a35fd795f
Badirenish <badirenish630@gmail.com>
2025-08-01 11:33:59 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,15 @@
-print("hello")
+a = int(input("Enter first Number:-"))
+b = int(input("Enter seconde Number:-"))
+
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+fname = input("Enter your fname:-")
+lname = input("enter your lname:- ")
+
+print(f"hello,{fname} {lname}! Welcom to the python program ")
